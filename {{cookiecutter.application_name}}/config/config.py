0dbe241541ed523121b6acac76c26ac121ab3645
Adam Shimali <adam.shimali@gmail.com>
2015-06-11 13:26:54 +0100

    default not secret key for dev

--- {{cookiecutter.application_name}}/config/config.py
+++ {{cookiecutter.application_name}}/config/config.py
@@ -4,11 +4,12 @@ import os
 class Config(object):
     APP_ROOT = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(Config):
     DEBUG = True
     WTF_CSRF_ENABLED = False
+     SECRET_KEY = os.environ.get('local-dev-not-secret')
 
 class TestConfig(Config):
     TESTING = True
